8d73f179a098b93b24c27caed9501942bcbbd890
Gosha Krupa <daniil.gavrilov.10@gmail.com>
2025-03-25 15:17:56 +0400

    feat: update class FinnhubWebsocket

--- etl/extract/extract_data.py
+++ etl/extract/extract_data.py
@@ -1,23 +1,27 @@
-from utils.finnhub_info import load_client, validate_ticker
 import websocket
-
 from src.utils import parse_config
-token = parse_config.get('/Users/gosakrupa/PycharmProjects/PythonProject/config/config.yaml')["api-key"]
+from utils.finnhub_info import load_client, validate_ticker
+import ssl
 
 class FinnhubWebsocket:
-    def __init__(self, ticker):
-        self.ticker = ticker
-        self.finnhub_client = load_client(token)
+    def __init__(self):
+        config = parse_config.get('/Users/gosakrupa/PycharmProjects/PythonProject/config/config.yaml')
+
+        self.token = config["api-key"]
+        self.ticker = config["ticker"]
+        self.finnhub_client = load_client(self.token)
+
         websocket.enableTrace(True)
-        ws = websocket.WebSocketApp(f"wss://ws.finnhub.io?token={token}",
+        self.ws = websocket.WebSocketApp(f"wss://ws.finnhub.io?token={self.token}",
                                     on_message=self.on_message,
                                     on_error=self.on_error,
                                     on_close=self.on_close)
-        ws.on_open = self.on_open
-        ws.run_forever()
+        self.ws.on_open = self.on_open
+        self.ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+
 
     def on_message(self, ws, message):
-        print(message)
+        print(dict(message))
 
     def on_error(self, ws, error):
         print(error)
@@ -27,5 +31,7 @@ class FinnhubWebsocket:
 
     def on_open(self, ws):
         if validate_ticker(self.finnhub_client, self.ticker):
-            send_dict = {"type": "subscribe", "symbol": f"{self.ticker}"}
-            ws.send('{"type":"subscribe","symbol":f"{ticker}"}')
+            self.ws.send(f'{{"type": "subscribe", "symbol": "{self.ticker}"}}')
+
+if __name__ == '__main__':
+    FinnhubWebsocket()
