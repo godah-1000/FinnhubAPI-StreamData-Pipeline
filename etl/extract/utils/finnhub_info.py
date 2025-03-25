bfdea15b79689ccc5a2286492eb7d427f2dccc5a
Gosha Krupa <daniil.gavrilov.10@gmail.com>
2025-03-25 14:20:28 +0400

    feat: add functions validate_ticker and load_client

--- etl/extract/utils/finnhub_info.py
+++ etl/extract/utils/finnhub_info.py
@@ -1,7 +1,12 @@
 import finnhub
-from src.utils import parse_config
 
-api_key = parse_config.get('/Users/gosakrupa/PycharmProjects/PythonProject/config/config.yaml')["api-key"]
-finnhub_client = finnhub.Client(api_key=api_key)
+def validate_ticker(finnhub_client, ticker):
+    symbol_lookup = finnhub_client.symbol_lookup(ticker)
+    if symbol_lookup['count'] > 0:
+        for items in symbol_lookup['result']:
+            if items['symbol'] == ticker:
+                return True
+    return False
 
-print(finnhub_client.symbol_lookup('BINANCE1:BTCUSDT'))
+def load_client(token):
+    return finnhub.Client(api_key=token)
